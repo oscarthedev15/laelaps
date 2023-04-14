c6a7b6d5276db326eb1ab9aba7a8472bda1942fc
Jesse <jesse.jacob@ey.com>
2023-04-14 17:54:38 -0400

    utility button

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ class App extends React.Component {
           </div>
         </div>
         <div className="d-flex justify-content-center">
-          <div className="title">
-            Laelaps
-          </div>
+          <div className="title">Laelaps</div>
         </div>
         <div className="access">
           <a href="https://www.example.com" className="access">
@@ -40,50 +38,18 @@ class App extends React.Component {
           </p>
         </div>
         <br></br>
-
-        {/* <div className="square">
+        <div className="square">
           <p className="textMain">
-            SHXDXW is an Ethereum-based utility token that powers the
-            decentralized chat application, acting as the primary medium of
-            exchange and interaction within the platform. It allows users to
-            access various features, such as sending messages, creating group
-            chats, and participating in secure voice calls. By using their
-            Ethereum wallets, users can maintain their anonymity while engaging
-            in various forms of communication. The decentralized chat
-            application harnesses the power of Ethereum's smart contract
-            technology and SHXDXW tokens to create a secure and private
-            environment for users. Instead of requiring users to pay tokens for
-            each communication, the platform simply mandates that users hold a
-            specific amount of SHXDXW tokens in their Ethereum wallets to access
-            and use the chat application. This approach makes the platform more
-            accessible and cost-effective while still maintaining its core
-            features and benefits. Once users meet the minimum SHXDXW token
-            holding requirement, they can freely communicate with others on the
-            platform. The smart contract technology processes each interaction,
-            encrypting messages and ensuring complete privacy and security. The
-            encrypted messages are then broadcasted to the recipient's Ethereum
-            wallet, providing a seamless and secure communication experience.
+            <div id="utility" className="access">
+              Utility
+            </div>
+            Laelaps is a project focused on optimizing users experience on the
+            blockchain. Through collecting and simplifying data, Laelaps is the
+            dog that thrives and improves as you fine tune your own methods with
+            him.
           </p>
         </div>
         <br></br>
-        <div className="square">
-          <p className="textMain">
-            Total Supply With a finite total supply of 100,000,000 tokens on the
-            Ethereum blockchain, SHXDXW is a deflationary cryptocurrency
-            designed for long-term growth potential. The team is committed to
-            buying back and burning tokens over time, ensuring a continuously
-            decreasing supply and increased scarcity. Tax Fee SHXDXW implements
-            a 4% tax fee on both buying and selling transactions, ensuring a
-            steady source of funding for marketing, development, and strategic
-            partnerships. This tax structure supports the project's ongoing
-            growth and contributes to its long-term success in the dynamic
-            crypto landscape. Token Limits SHXDXW ensures fairness by
-            implementing a 2% maximum transaction limit and a 4% maximum wallet
-            limit. These measures prevent early investors from dominating the
-            supply, promoting a more equitable distribution of tokens for
-            participants entering the market around the same time.
-          </p>
-        </div> */}
       </div>
     );
   }
--- src/components/Navbar/MyNavbar.js
+++ src/components/Navbar/MyNavbar.js
@@ -1,12 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
+import React from "react";
+
 
 const Navigation = () => {
+    function utilityClick() {
+          document
+            .getElementById("utility")
+            .scrollIntoView({ behavior: "smooth" });
+    }
+
+
     return (
       <div className="container">
         <button className="button">Home</button>
         <button className="button">Chart</button>
-        <button className="button">Utility</button>
+        <button className="button" onClick={utilityClick}>Utility</button>
         <button className="button">Telegram</button>
         <button className="button">DAO</button>
       </div>
