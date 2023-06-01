538f59ad4122c6f31ac47c3ffac8fd76fe547037
Jesse <jesse.jacob@ey.com>
2023-06-01 02:07:08 -0400

    added image

--- laelaps/pages/index.js
+++ laelaps/pages/index.js
@@ -12,8 +12,6 @@ export default function Home() {
 
   return (
     <div className={styles.background}>
-      <ConnectWallet />
-
       <div className={styles.bigBox}>
         <div className={styles.logoBox}>
           <img
