d21a642d83be2442fbdf91e68eff43ec1fbb0e38
Jesse <jesse.jacob@ey.com>
2023-06-01 20:03:37 -0400

    added gif to mint page

--- laelaps/pages/MintPage.js
+++ laelaps/pages/MintPage.js
@@ -8,12 +8,19 @@ export default function Utility() {
     <div className={styles.square}>
       <ConnectWallet />
       <br />
+      <div className={styles.gif}>
+        <img
+          src="https://cloudflare-ipfs.com/ipfs/QmXT48ANVqBHMbCP9kYDtVezh6MqPLPKw95oETKKdxcdA6"
+          className={styles.gifImage}
+        />
+      </div>
+      <br />
       <div className={styles.title}>Mint Your Master Key</div>
       <br />
 
       <div className={styles.textMain}>
-        Every Master Key buy will automatically trigger a purchase of $LAELAPS
-        and immediately burn the purchased tokens. 
+        Coming Soon! Every Master Key purchase will automatically trigger a
+        purchase of $LAELAPS and immediately burn the purchased tokens.
       </div>
     </div>
   );
