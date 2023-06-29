7657a86764e115b1773e9e40e60bf65864657f16
Jesse <jesse.jacob@ey.com>
2023-06-29 14:34:22 -0400

    apostrophe issue

--- laelaps/pages/Utility.js
+++ laelaps/pages/Utility.js
@@ -6,8 +6,8 @@ export default function Utility() {
       <br />
 
       <div className={styles.textMain}>
-        Laelaps is a project that aims to enhance users' experience on the
-        blockchain. By collecting and simplifying data, Laelaps acts as a
+        Laelaps is a project that aims to enhance users`&apos;` experience on
+        the blockchain. By collecting and simplifying data, Laelaps acts as a
         trusted companion that thrives and improves alongside you as you
         fine-tune your own methods.
       </div>
