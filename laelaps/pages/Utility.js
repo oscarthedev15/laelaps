d685b2f022e71cea2e74e2339d817023cd7150bb
Jesse <jesse.jacob@ey.com>
2023-06-29 14:54:02 -0400

    apostroe

--- laelaps/pages/Utility.js
+++ laelaps/pages/Utility.js
@@ -6,7 +6,7 @@ export default function Utility() {
       <br />
 
       <div className={styles.textMain}>
-        Laelaps is a project that aims to enhance users`&apos;` experience on
+        Laelaps is a project that aims to enhance users&apos; experience on
         the blockchain. By collecting and simplifying data, Laelaps acts as a
         trusted companion that thrives and improves alongside you as you
         fine-tune your own methods.
