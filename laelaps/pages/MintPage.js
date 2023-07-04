5de8068498bec21491b467bafd8a9d1ea1ebe49b
Jesse <jesse.jacob@ey.com>
2023-07-04 01:07:36 -0400

    all keys

--- laelaps/pages/MintPage.js
+++ laelaps/pages/MintPage.js
@@ -167,39 +167,45 @@ export default function Utility() {
       <br />
       <div className={styles.title}>Mint Your Master Key</div>
       <br /> */}
-      <div className={styles.gifRow}>
-        <div className={styles.gif}>
-          <img
-            src="https://cloudflare-ipfs.com/ipfs/QmRLrqVV4mzQyEZfJYv4KjfwUxZAxeh7EEMTkghPwcH5Hf"
-            className={styles.gifImage}
-          />
-        </div>
-
-        <div className={styles.gif}>
-          <img
-            src="https://cloudflare-ipfs.com/ipfs/QmW8Tow79oWqyGBYTe6HUmT8zLqb39oVqRFqBRL2imS28z"
-            className={styles.gifImage}
-          />
-        </div>
-
-        <div className={styles.gif}>
-          <img
-            src="https://cloudflare-ipfs.com/ipfs/QmWdjfJb4z3m9YkEvvRb3DY85KCkFhSEGF6YHVWB9qZe8B"
-            className={styles.gifImage}
-          />
-        </div>
-
-        <div className={styles.gif}>
-          <img
-            src="https://cloudflare-ipfs.com/ipfs/QmaVmzp7Ln8YQbZwu5ahqfWdjdcn9KEdDBctmMMAng2RfJ"
-            className={styles.gifImage}
-          />
-        </div>
-      </div>
-
       <div className={styles.textMain}>
         Master Key mint paused! Announcement coming soon....
       </div>
+        <div className={styles.gifRow}>
+          <div className={styles.gif}>
+            <img
+              src="https://cloudflare-ipfs.com/ipfs/QmRLrqVV4mzQyEZfJYv4KjfwUxZAxeh7EEMTkghPwcH5Hf"
+              className={styles.gifImage}
+            />
+          </div>
+
+          <div className={styles.gif}>
+            <img
+              src="https://cloudflare-ipfs.com/ipfs/QmW8Tow79oWqyGBYTe6HUmT8zLqb39oVqRFqBRL2imS28z"
+              className={styles.gifImage}
+            />
+          </div>
+
+          <div className={styles.gif}>
+            <img
+              src="https://cloudflare-ipfs.com/ipfs/QmWdjfJb4z3m9YkEvvRb3DY85KCkFhSEGF6YHVWB9qZe8B"
+              className={styles.gifImage}
+            />
+          </div>
+
+          <div className={styles.gif}>
+            <img
+              src="https://cloudflare-ipfs.com/ipfs/QmaVmzp7Ln8YQbZwu5ahqfWdjdcn9KEdDBctmMMAng2RfJ"
+              className={styles.gifImage}
+            />
+          </div>
+
+          <div className={styles.gif}>
+            <img
+              src="https://cloudflare-ipfs.com/ipfs/QmZ4reQiStQACTLYncxo7nsBHrB4hC1kzweu16b7SjeHWY"
+              className={styles.gifImage}
+            />
+          </div>
+        </div>
     </div>
   );
 }
