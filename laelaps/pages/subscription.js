46e75d2d2897b9af1065b98df6df24bbda761e80
Shelby Theisen <shelbytheisen@Shelbys-MacBook-Air.local>
2023-09-17 23:21:19 -0400

    new button, badge, and box styles as well as fonts + connect wallet CTA redesign

--- laelaps/pages/subscription.js
+++ laelaps/pages/subscription.js
@@ -4,7 +4,10 @@ import React, {
 } from "react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 import styles from "../styles/Subscription.module.css";
-import { useSwitchChain } from "@thirdweb-dev/react";
+import {
+  useSwitchChain,
+  useConnectionStatus,
+} from "@thirdweb-dev/react";
 import { useNetworkMismatch } from "@thirdweb-dev/react";
 import {
   Ethereum,
@@ -29,6 +32,8 @@ export default function Utility() {
     {}
   );
   const switchChain = useSwitchChain();
+  const connectionStatus =
+    useConnectionStatus();
   const isMismatched =
     useNetworkMismatch();
   const userAddress = useAddress();
@@ -81,9 +86,27 @@ export default function Utility() {
     userAddress,
   ]);
 
+  if (
+    connectionStatus ===
+      "disconnected" ||
+    connectionStatus === "unknown"
+  ) {
+    return (
+      <div className="flex justify-center items-center h-[80vh] w-full">
+        <div className="flex flex-col gap-6 items-center justify-center min-w-[400px] p-6 bg-white border-2 border-[#9a4737] rounded-3xl shadow-[5px_5px_0px_0px_rgba(146,69,53,1)] ">
+          <p className="text-black text-lg w-3/4 text-center font-prozaReg">
+            Please connect your wallet
+            to continue.
+          </p>
+          <ConnectWallet className="!text-2xl !bg-[#cc624a3b] !text-[#cc624a] !font-quattBold !px-4 hover:!bg-[#cc624a47]" />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={styles.square}>
       <ConnectWallet />
+
       <br />
       <div className=" w-4/5">
         {times.message ? (
@@ -96,22 +119,52 @@ export default function Utility() {
           <>
             <div className="flex flex-col gap-8 w-full">
               <div className="flex flex-row gap-8 items-center mt-5">
-                <div className="text-3xl text-[#8b0000] font-bold">
+                <div className=" text-5xl text-[#682c22ff] font-quattBold">
                   Your Membership
                 </div>
                 <span
-                  className={`inline-flex uppercase font-bold tracking-widest items-center rounded-full px-4 py-2 text-lg ${
+                  className={`inline-flex font-prozaSemi font-bold tracking-wider uppercase items-center gap-x-2 rounded-full px-4 py-2 text-lg ${
                     times?.statusObj
                       ?.status ===
                       "Expired" &&
-                    "bg-red-500 ring-red-500/10 text-white ring-1 ring-inset"
+                    "bg-red-100/90 ring-red-600/10 text-red-700 ring-1 ring-inset"
                   } ${
                     times?.statusObj
                       ?.status ===
                       "Active" &&
-                    "bg-green-500 ring-green-500/10 text-white ring-1 ring-inset"
+                    "bg-green-100/90 ring-green-600/10 text-green-700 ring-1 ring-inset"
                   }`}
                 >
+                  {times?.statusObj
+                    ?.status ===
+                  "Expired" ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="w-5 h-5"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="w-5 h-5"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  )}
+
                   {
                     times?.statusObj
                       ?.status
@@ -119,13 +172,8 @@ export default function Utility() {
                 </span>
               </div>
               <div>
-                <div className="text-xl text-black font-bold -mt-6">
-                  <a
-                    href="google.com"
-                    className="text-black underline"
-                  >
-                    Joined
-                  </a>{" "}
+                <div className="text-base text-black font-prozaItal -mt-7">
+                  Joined{" "}
                   {times.formattedTime}
                 </div>
               </div>
@@ -153,7 +201,7 @@ export default function Utility() {
                         transaction link
                       </div>
                     </div> */}
-                    <div className="flex flex-col gap-6 items-center self-center border-2 border-[#8b0000] rounded-3xl shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] p-6 mx-16">
+                    <div className="flex flex-col gap-6 items-center self-center max-16 p-6 bg-white font-prozaReg border-2 border-[#9a4737] rounded-3xl shadow-[5px_5px_0px_0px_rgba(146,69,53,1)] ">
                       {times?.statusObj
                         ?.status ===
                       "Expired" ? (
@@ -253,7 +301,7 @@ export default function Utility() {
                             );
                             location.reload();
                           }}
-                          className="!bg-[#8b0000] !text-white !text-3xl !rounded-3xl !px-10"
+                          className="!bg-[#cc624a3b] !text-[#cc624a] !text-3xl !font-quattBold !rounded-3xl hover:!bg-[#cc624a47] !px-10"
                         >
                           Make Payment
                         </Web3Button>
