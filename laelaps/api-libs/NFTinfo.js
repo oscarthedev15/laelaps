b7de890911d985df4a7d870c3e91f582dbe18887
oscar <jesserjacob99@gmail.com>
2023-10-05 11:50:12 -0400

    telegram invite and sub contract

--- laelaps/api-libs/NFTinfo.js
+++ laelaps/api-libs/NFTinfo.js
@@ -1,15 +1,11 @@
 // Setup: npm install alchemy-sdk
-import {
-  Alchemy,
-  Network,
-  fromHex,
-} from "alchemy-sdk";
+import { Alchemy, Network, fromHex } from "alchemy-sdk";
 const { ethers } = require("ethers");
 import Subscription from "../models/subscription.js";
 import mongoose from "./db.js";
+import { sendZeusInvite } from "./telegram.js";
 
-const REWARD_ADDRESS =
-  "0x48bffd60686b8259887862d0e73ac2087d446a5f";
+const REWARD_ADDRESS = "0x48bffd60686b8259887862d0e73ac2087d446a5f";
 // const SUBSCRIPTION_COST = 0.00000000000001;
 
 const NETWORK = Network.ETH_GOERLI;
@@ -33,13 +29,7 @@ const MONTHS = [
   "Dec",
 ];
 
-export async function getNFTinfo(
-  userAddress,
-  nftAddress,
-  chatId,
-  bot,
-  price
-) {
+export async function getNFTinfo(userAddress, nftAddress, chatId, bot, price) {
   const config = {
     apiKey: process.env.ALCHEMY_TOKEN,
     network: NETWORK,
@@ -47,45 +37,32 @@ export async function getNFTinfo(
   const alchemy = new Alchemy(config);
 
   //_____________________________Code for NFT receiving: __________________________________
-  const res =
-    await alchemy.core.getAssetTransfers(
-      {
-        fromBlock: "0x0",
-        toAddress: userAddress,
-        excludeZeroValue: true,
-        category: ["erc1155"],
-      }
-    );
+  const res = await alchemy.core.getAssetTransfers({
+    fromBlock: "0x0",
+    toAddress: userAddress,
+    excludeZeroValue: true,
+    category: ["erc1155"],
+  });
 
   let txns = res.transfers.filter(
-    (txn) =>
-      txn.rawContract.address ==
-      nftAddress
+    (txn) => txn.rawContract.address == nftAddress
   );
 
-  if (txns.length == 0)
-    return { message: "Not a holder" };
+  if (txns.length == 0) return { message: "Not a holder" };
   const blockNumHex = txns[0].blockNum;
-  const blockNumDecimal = fromHex(
-    blockNumHex
-  );
+  const blockNumDecimal = fromHex(blockNumHex);
   const txnHash = txns[0].hash;
-  const provider =
-    new ethers.providers.AlchemyProvider(
-      providerNetwork,
-      process.env.ALCHEMY_TOKEN
-    );
-  const unixTimestamp = (
-    await provider.getBlock(
-      blockNumDecimal
-    )
-  ).timestamp; //UNIX!
+  const provider = new ethers.providers.AlchemyProvider(
+    providerNetwork,
+    process.env.ALCHEMY_TOKEN
+  );
+  const unixTimestamp = (await provider.getBlock(blockNumDecimal)).timestamp; //UNIX!
   // __________________________________________________________________________________
 
   const statusObj = await getStatus(
     userAddress,
     unixTimestamp,
-    blockNumHex, 
+    blockNumHex,
     price
   );
 
@@ -93,19 +70,13 @@ export async function getNFTinfo(
     blockNum: blockNumDecimal,
     txnHash: txnHash,
     timestamp: unixTimestamp,
-    mintDateObj: unixToHumanDate(
-      unixTimestamp
-    ),
+    mintDateObj: unixToHumanDate(unixTimestamp),
     statusObj: statusObj,
   };
 
-  const addressInDB =
-    await getDocuments(userAddress);
+  const addressInDB = await getDocuments(userAddress);
 
-  if (
-    chatId &&
-    addressInDB.length == 0
-  ) {
+  if (chatId && addressInDB.length == 0) {
     Subscription.createSubscription(
       chatId,
       userAddress,
@@ -114,52 +85,37 @@ export async function getNFTinfo(
       statusObj.status
     )
       .then((newSubscription) => {
-        console.log(
-          "New subscription created:",
-          newSubscription
-        );
+        console.log("New subscription created:", newSubscription);
         // WRITE TELEGRAM MESSAGE BACK
+        sendZeusInvite(chatId, bot);
       })
       .catch((error) => {
-        console.error(
-          "Error creating subscription:",
-          error
-        );
+        console.error("Error creating subscription:", error);
       });
   } else if (addressInDB.length > 0) {
     if (
-      addressInDB[0].status !=
-        statusObj.status ||
-      addressInDB[0].validThru !=
-        statusObj.validThru
+      addressInDB[0].status != statusObj.status ||
+      addressInDB[0].validThru != statusObj.validThru
     ) {
       console.log("Need to update!!!!");
-      if (
-        addressInDB[0].status !=
-        statusObj.status
-      ) {
-        Subscription.updateDocument(
-          userAddress,
-          "status",
-          statusObj.status
-        )
+      if (addressInDB[0].status != statusObj.status) {
+        if (
+          addressInDB[0].status == "Expired" &&
+          statusObj.status == "Active"
+        ) {
+          // WRITE TELEGRAM MESSAGE BACK
+          sendZeusInvite(chatId, bot);
+        }
+        Subscription.updateDocument(userAddress, "status", statusObj.status)
           .then((updatedDocument) => {
             if (updatedDocument) {
-              console.log(
-                "Updated Document:",
-                updatedDocument
-              );
+              console.log("Updated Document:", updatedDocument);
             } else {
-              console.log(
-                "Document not found."
-              );
+              console.log("Document not found.");
             }
           })
           .catch((error) => {
-            console.error(
-              "Error updating document:",
-              error
-            );
+            console.error("Error updating document:", error);
           });
       } else {
         Subscription.updateDocument(
@@ -169,21 +125,13 @@ export async function getNFTinfo(
         )
           .then((updatedDocument) => {
             if (updatedDocument) {
-              console.log(
-                "Updated Document:",
-                updatedDocument
-              );
+              console.log("Updated Document:", updatedDocument);
             } else {
-              console.log(
-                "Document not found."
-              );
+              console.log("Document not found.");
             }
           })
           .catch((error) => {
-            console.error(
-              "Error updating document:",
-              error
-            );
+            console.error("Error updating document:", error);
           });
       }
     }
@@ -192,25 +140,13 @@ export async function getNFTinfo(
   return returnObj;
 }
 
-async function getStatus(
-  userAddress,
-  mintUnixTimetamp,
-  blockNum, 
-  price
-) {
+async function getStatus(userAddress, mintUnixTimetamp, blockNum, price) {
   // NFT has not expired yet!
-  const isLessThanDays =
-    isLessThanDaysAgo(
-      mintUnixTimetamp,
-      DAYS_AGO
-    );
+  const isLessThanDays = isLessThanDaysAgo(mintUnixTimetamp, DAYS_AGO);
   if (isLessThanDays) {
     return {
       status: "Active",
-      validThru: unixTimestampInFuture(
-        mintUnixTimetamp,
-        DAYS_AGO
-      ),
+      validThru: unixTimestampInFuture(mintUnixTimetamp, DAYS_AGO),
     };
   }
 
@@ -219,45 +155,32 @@ async function getStatus(
     network: NETWORK,
   };
   const alchemy = new Alchemy(config);
-  const res =
-    await alchemy.core.getAssetTransfers(
-      {
-        fromBlock: blockNum,
-        toAddress: REWARD_ADDRESS,
-        fromAddress: userAddress,
-        excludeZeroValue: true,
-        category: ["external"],
-      }
-    );
-    
-  
+  const res = await alchemy.core.getAssetTransfers({
+    fromBlock: blockNum,
+    toAddress: REWARD_ADDRESS,
+    fromAddress: userAddress,
+    excludeZeroValue: true,
+    category: ["external"],
+  });
+
   const weiValue = ethers.utils.parseUnits(price.toString(), "wei");
   const etherValue = ethers.utils.formatEther(weiValue);
 
   let txns = res.transfers.filter(
-    (txn) =>
-      txn.value >= etherValue &&
-      txn.asset === "ETH"
+    (txn) => txn.value >= etherValue && txn.asset === "ETH"
   );
 
   if (txns.length != 0) {
-    const blockNumPayment = fromHex(
-      txns[txns.length - 1].blockNum
+    const blockNumPayment = fromHex(txns[txns.length - 1].blockNum);
+
+    const txnHash = txns[txns.length - 1].hash;
+
+    const provider = new ethers.providers.AlchemyProvider(
+      providerNetwork,
+      process.env.ALCHEMY_TOKEN
     );
-
-    const txnHash =
-      txns[txns.length - 1].hash;
-
-    const provider =
-      new ethers.providers.AlchemyProvider(
-        providerNetwork,
-        process.env.ALCHEMY_TOKEN
-      );
-    const unixTimestampPayment = (
-      await provider.getBlock(
-        blockNumPayment
-      )
-    ).timestamp; //UNIX!
+    const unixTimestampPayment = (await provider.getBlock(blockNumPayment))
+      .timestamp; //UNIX!
 
     if (
       areTimestampsWithinDaysApart(
@@ -266,70 +189,43 @@ async function getStatus(
         DAYS_AGO
       )
     ) {
-      let validThru =
-        unixTimestampInFuture(
-          unixTimestampPayment,
-          DAYS_AGO
-        );
+      let validThru = unixTimestampInFuture(unixTimestampPayment, DAYS_AGO);
       // validThru =
       //   unixToHumanDate(validThru);
 
-      const lastPaymentObj =
-        unixToHumanDate(
-          unixTimestampPayment
-        );
+      const lastPaymentObj = unixToHumanDate(unixTimestampPayment);
       const statusObj = {
-        unixTimestampPayment:
-          unixTimestampPayment,
-        lastPaymentDateObj:
-          lastPaymentObj,
+        unixTimestampPayment: unixTimestampPayment,
+        lastPaymentDateObj: lastPaymentObj,
         blockPayment: blockNumPayment,
         txnHash: txnHash,
         status: "Active",
         validThru: validThru,
-        validThruHumanDate:
-          unixToHumanDateTime(
-            validThru
-          ),
+        validThruHumanDate: unixToHumanDateTime(validThru),
       };
       return statusObj;
     } else {
       return {
         status: "Expired",
-        validThru:
-          unixTimestampInFuture(
-            mintUnixTimetamp,
-            DAYS_AGO
-          ),
+        validThru: unixTimestampInFuture(mintUnixTimetamp, DAYS_AGO),
       };
     }
   } else {
     return {
       status: "Expired",
-      validThru: unixTimestampInFuture(
-        mintUnixTimetamp,
-        DAYS_AGO
-      ),
+      validThru: unixTimestampInFuture(mintUnixTimetamp, DAYS_AGO),
     };
   }
 }
 
-function unixToHumanDate(
-  unixTimestamp
-) {
-  const milliseconds =
-    unixTimestamp * 1000; // Convert seconds to milliseconds
-  const dateObject = new Date(
-    milliseconds
-  );
+function unixToHumanDate(unixTimestamp) {
+  const milliseconds = unixTimestamp * 1000; // Convert seconds to milliseconds
+  const dateObject = new Date(milliseconds);
 
   const y = dateObject.getFullYear();
-  const m =
-    MONTHS[dateObject.getMonth()];
+  const m = MONTHS[dateObject.getMonth()];
 
-  const d = dateObject
-    .getDate()
-    .toString();
+  const d = dateObject.getDate().toString();
 
   return {
     day: d,
@@ -338,108 +234,53 @@ function unixToHumanDate(
   };
 }
 
-function unixToHumanDateTime(
-  unixTimestamp
-) {
-  const milliseconds =
-    unixTimestamp * 1000; // Convert seconds to milliseconds
-  const dateObject = new Date(
-    milliseconds
-  );
+function unixToHumanDateTime(unixTimestamp) {
+  const milliseconds = unixTimestamp * 1000; // Convert seconds to milliseconds
+  const dateObject = new Date(milliseconds);
 
   const year = dateObject.getFullYear();
-  const month =
-    MONTHS[dateObject.getMonth()];
+  const month = MONTHS[dateObject.getMonth()];
 
-  const day = dateObject
-    .getDate()
-    .toString();
-  const hours = dateObject
-    .getHours()
-    .toString()
-    .padStart(2, "0");
-  const minutes = dateObject
-    .getMinutes()
-    .toString()
-    .padStart(2, "0");
-  const seconds = dateObject
-    .getSeconds()
-    .toString()
-    .padStart(2, "0");
+  const day = dateObject.getDate().toString();
+  const hours = dateObject.getHours().toString().padStart(2, "0");
+  const minutes = dateObject.getMinutes().toString().padStart(2, "0");
+  const seconds = dateObject.getSeconds().toString().padStart(2, "0");
 
-  const time =
-    dateObject.toLocaleTimeString(
-      "en-US",
-      { timeStyle: "short" }
-    );
+  const time = dateObject.toLocaleTimeString("en-US", { timeStyle: "short" });
 
   const formattedDateTime = `${month} ${day}, ${year} ${time}`;
   return formattedDateTime;
 }
 
-function isLessThanDaysAgo(
-  unixTimestamp,
-  daysAgo
-) {
-  const currentTimestamp = Math.floor(
-    Date.now() / 1000
-  ); // Get current Unix timestamp
+function isLessThanDaysAgo(unixTimestamp, daysAgo) {
+  const currentTimestamp = Math.floor(Date.now() / 1000); // Get current Unix timestamp
   const secondsInADay = 24 * 60 * 60; // Number of seconds in a day
 
-  return (
-    currentTimestamp - unixTimestamp <
-    daysAgo * secondsInADay
-  );
+  return currentTimestamp - unixTimestamp < daysAgo * secondsInADay;
 }
 
-function areTimestampsWithinDaysApart(
-  timestamp1,
-  timestamp2,
-  daysApart
-) {
+function areTimestampsWithinDaysApart(timestamp1, timestamp2, daysApart) {
   const secondsPerDay = 24 * 60 * 60; // Number of seconds in a day
-  const timeDifference = Math.abs(
-    timestamp1 - timestamp2
-  );
-  const daysDifference =
-    timeDifference / secondsPerDay;
-  return (
-    daysDifference <=
-    Math.abs(daysApart)
-  );
+  const timeDifference = Math.abs(timestamp1 - timestamp2);
+  const daysDifference = timeDifference / secondsPerDay;
+  return daysDifference <= Math.abs(daysApart);
 }
 
-function unixTimestampInFuture(
-  baseTimestamp,
-  daysInFuture
-) {
-  const baseTimestampMilliseconds =
-    baseTimestamp * 1000;
-  const millisecondsInDay =
-    24 * 60 * 60 * 1000;
-  const millisecondsToAdd =
-    daysInFuture * millisecondsInDay;
+function unixTimestampInFuture(baseTimestamp, daysInFuture) {
+  const baseTimestampMilliseconds = baseTimestamp * 1000;
+  const millisecondsInDay = 24 * 60 * 60 * 1000;
+  const millisecondsToAdd = daysInFuture * millisecondsInDay;
   const futureTimestampMilliseconds =
-    baseTimestampMilliseconds +
-    millisecondsToAdd;
-  const futureTimestampSeconds =
-    Math.floor(
-      futureTimestampMilliseconds / 1000
-    );
+    baseTimestampMilliseconds + millisecondsToAdd;
+  const futureTimestampSeconds = Math.floor(futureTimestampMilliseconds / 1000);
   return futureTimestampSeconds;
 }
 
 async function getDocuments(address) {
   try {
-    const documents =
-      await Subscription.findByAddress(
-        address
-      ).exec();
+    const documents = await Subscription.findByAddress(address).exec();
     return documents;
   } catch (error) {
-    console.error(
-      "Error fetching documents:",
-      error
-    );
+    console.error("Error fetching documents:", error);
   }
 }
---
+++ laelaps/api-libs/telegram.js
@@ -0,0 +1,54 @@
+
+
+import { Telegraf } from "telegraf";
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot1 = new Telegraf(process.env.BOT1_TOKEN);
+const bot2 = new Telegraf(process.env.BOT2_TOKEN);
+const bot3 = new Telegraf(process.env.BOT3_TOKEN);
+const bot4 = new Telegraf(process.env.BOT4_TOKEN);
+const bot5 = new Telegraf(process.env.BOT5_TOKEN);
+const bot6 = new Telegraf(process.env.BOT6_TOKEN);
+const bot7 = new Telegraf(process.env.BOT7_TOKEN);
+const bot8 = new Telegraf(process.env.BOT8_TOKEN);
+const bot9 = new Telegraf(process.env.BOT9_TOKEN);
+const bot10 = new Telegraf(process.env.BOT10_TOKEN);
+const bot11 = new Telegraf(process.env.BOT11_TOKEN);
+const bot12 = new Telegraf(process.env.BOT12_TOKEN);
+const bot13 = new Telegraf(process.env.BOT13_TOKEN);
+
+const noBot = {
+  1: bot,
+  2: bot1,
+  3: bot2,
+  4: bot3,
+  5: bot4,
+  6: bot5,
+  7: bot6,
+  8: bot7,
+  9: bot8,
+  10: bot9,
+  11: bot10,
+  12: bot11,
+  13: bot12,
+  14: bot13,
+};
+
+
+export async function sendZeusInvite(chatid, bot) {
+    const zeusId = -1001987532423
+        // ctx.answerCbQuery();
+        // const chatId = ctx.match[0].split("-")[1];
+        // var bot = noBot[ctx.match[0].split("-")[2]];
+        var zeusBot = noBot[1]
+        try{
+            var link = (await zeusBot.telegram.createChatInviteLink(zeusId, {member_limit: 1, expire_date: Date.now()+60000}))
+        }catch(err){
+            console.log(err)
+            return;
+        }
+        console.log(link)
+        await zeusBot.telegram.sendMessage(chatid, `Your unique zeus invite link is ${link.invite_link}`)
+
+}
+
+
