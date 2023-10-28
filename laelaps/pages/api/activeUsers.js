5ebb9ff606c2fc2814779ac5ed998e9881bbece2
oscar <jesserjacob99@gmail.com>
2023-10-28 13:35:00 -0400

    cors

--- laelaps/pages/api/activeUsers.js
+++ laelaps/pages/api/activeUsers.js
@@ -1,12 +1,7 @@
 import { getActiveUsers } from "../../api-libs/activeUsers";
-import Cors from 'cors';
 
-const cors = Cors({
-  methods: ['GET'], // Adjust the allowed HTTP methods as needed
-});
 
 export default async function handler(req, res) {
-  await cors(req, res);
 
   if (req.method !== "GET") {
     res.status(405).json({ error: { message: "Method Not Allowed" } });
